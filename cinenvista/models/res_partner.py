37874abca34312ba4045d99645c3e0a7ac1f74b6
marcosmgcandas19 <marcosmgcandas19@gmail.com>
2026-02-05 10:38:35 +0100

    Tarea 4 : Contador de reservas

--- cinenvista/models/res_partner.py
+++ cinenvista/models/res_partner.py
@@ -1,6 +1,13 @@
-from odoo import models, fields
+from odoo import models, fields, api
 
 class ResPartner(models.Model):
     _inherit = 'res.partner'
 
     fax = fields.Char(string='Fax')
+    reservation_ids = fields.One2many('cinenvista.reservation', 'partner_id', string='Reservas')
+    reservation_count = fields.Integer(string='Número de reservas', compute='_compute_reservation_count')
+
+    @api.depends('reservation_ids')
+    def _compute_reservation_count(self):
+        for rec in self:
+            rec.reservation_count = len(rec.reservation_ids)
